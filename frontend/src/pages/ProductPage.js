6c15162eb2e54bb3748d2e5b3b5e5e5432385b46
Michael Ihde <michael.ihde@easylearn.ch>
2021-09-06 11:29:54 +0200

    Added Product Page

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Card, Image, Button } from 'react-bootstrap'
+import RatingStars from '../components/RatingStars'
+import products from '../products'
 
-const ProductPage = () => {
+const ProductPage = ({ match }) => {
+
+    const product = products.find(p => p._id === match.params.id)
+
     return (
         <div>
-            <h>Product Page</h>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} fluid />
+                </Col>
+                <Col md={6}>
+                    <Card>
+                        <Card.Header>
+                            <h3>{product.name}</h3>
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                {product.description}
+                            </Card.Text>
+                            <hr />
+                            <div className="d-flex justify-content-between my-2 lead">
+                                <RatingStars rating={product.rating} ratings={product.numReviews} />
+                                <Card.Text>
+                                    <strong>CHF {product.price}</strong>
+                                </Card.Text>
+                            </div>
+                            <Button className="form-control">In den Korb</Button>
+                        </Card.Body>
+                    </Card>
+                
+                </Col>
+            </Row>
         </div>
     )
 }
