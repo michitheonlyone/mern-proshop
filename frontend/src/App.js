1e171f9ffc40efe79033d6d065416c2518dfe776
Michael Ihde <michael.ihde@easylearn.ch>
2021-09-06 09:03:41 +0200

    Added basic structure and Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Container } from 'react-bootstrap';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-
+        <Header />
+        <main>
+            <Container>
+            </Container>
+        </main>
+        <Footer />
     </div>
   );
 }
