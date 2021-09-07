ceb2078cf3eb4e29194b414a70fb3c6e4baab83b
Michael Ihde <michael.ihde@easylearn.ch>
2021-09-07 16:35:16 +0200

    added redux store, reducers and actions

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,49 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Card, Image, Button } from 'react-bootstrap'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import RatingStars from '../components/RatingStars'
-import axios from 'axios'
+import { listProductDetails } from '../actions/productActions'
 
 const ProductPage = ({ match }) => {
-  const [product, setProduct] = useState([])
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`)
-      setProduct(data)
-    }
-
-    fetchProduct()
-  }, [])
+    dispatch(listProductDetails(match.params.id))
+  }, [dispatch, match])
 
   return (
     <>
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} fluid />
-        </Col>
-        <Col md={6}>
-          <Card>
-            <Card.Header>
-              <h3>{product.name}</h3>
-            </Card.Header>
-            <Card.Body>
-              <Card.Text>{product.description}</Card.Text>
-              <hr />
-              <div className='d-flex justify-content-between my-2 lead'>
-                <RatingStars
-                  rating={product.rating}
-                  ratings={product.numReviews}
-                />
-                <Card.Text>
-                  <strong>CHF {product.price}</strong>
-                </Card.Text>
-              </div>
-              <Button className='form-control'>In den Korb</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} fluid />
+          </Col>
+          <Col md={6}>
+            <Card>
+              <Card.Header>
+                <h3>{product.name}</h3>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>{product.description}</Card.Text>
+                <hr />
+                <div className='d-flex justify-content-between my-2 lead'>
+                  <RatingStars
+                    rating={product.rating}
+                    ratings={product.numReviews}
+                  />
+                  <Card.Text>
+                    <strong>CHF {product.price}</strong>
+                  </Card.Text>
+                </div>
+                <Button className='form-control'>In den Korb</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   )
 }
